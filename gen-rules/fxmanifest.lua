a148f94c8366c2ff5792d584502adc4daa618070
Gen <88355025+GenTheDev@users.noreply.github.com>
2021-08-10 16:32:04 -0400

    Update fxmanifest.lua

--- gen-rules/fxmanifest.lua
+++ gen-rules/fxmanifest.lua
@@ -1,7 +1,6 @@
 --{ COPYRIGHT (C) GEN 'AGB DEVELOPMENT' --}
 --{ ALL RIGHTS RESERVED --}
 --{ DO NOT REUPLOAD WITHOUT PERMISSON --}
---{ DO NOT EDIT WITHOUT PERMISSION --}
 --{ Discord: Geny#8736 --}
 
 fx_version 'cerulean'
